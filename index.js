058f4704ef73d5a7caaa8d3b484cb794b4a3e35e
Thejus Paul <thejuspaul@pm.me>
2023-02-26 12:34:19 +0530

    Refactored the code

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const github = require("@actions/github");
 
-const BUMP_TYPES = ["major", "minor", "patch"];
-
 const containsBumpTypeLabel = (labels) => {
+  const bumpTypes = ["major", "minor", "patch"];
   const labelsArray = labels.split(",");
 
   if (labelsArray.length === 0 || labelsArray[0] === "") {
     return false;
   }
 
-  return BUMP_TYPES.some((bump_type) => labelsArray.includes(bump_type));
+  return bumpTypes.some((bump_type) => labelsArray.includes(bump_type));
 };
 
 const createBranch = async (octokit, context, branch) => {
@@ -50,30 +49,30 @@ const run = async () => {
       );
     }
 
-    console.log("\nInstalling the gem...");
+    core.debug("\nInstalling the gem...");
     await exec.exec("gem install -q --silent bump_gem_version");
 
-    console.log("\nBefore bump, the gem version was:");
+    core.debug("\nBefore bump, the gem version was:");
     await exec.exec("bump_gem_version current");
 
-    console.log("\nBumping gem version...");
+    core.debug("\nBumping gem version...");
     await exec.exec(`bump_gem_version labels ${labels}`);
 
-    console.log("\nSuccessfully bumped gem version! 🎉");
+    core.debug("\nSuccessfully bumped gem version! 🎉");
 
-    console.log("\nAfter bump, the gem version is:");
+    core.debug("\nAfter bump, the gem version is:");
     await exec.exec("bump_gem_version current");
 
-    console.log("\nCommitting and pushing the new version...");
+    core.debug("\nCommitting and pushing the new version...");
     const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
     const branch = core.getInput("branch");
     const context = github.context;
-    const octokit = github.getOctokit(token);
 
     core.debug(`Creating branch ${branch}`);
     const isCreated = await createBranch(octokit, context, branch);
 
-    core.setOutput("created", Boolean(isCreated));
+    console.log("created", isCreated);
   } catch (error) {
     core.setFailed(error.message);
   }
