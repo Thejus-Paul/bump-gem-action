8b65656ef2ed30b2f8a55c6fa7c0110820b91f18
Thejus Paul <thejuspaul@pm.me>
2023-02-26 15:40:23 +0530

    Added the ability to create a commit

--- dist/index.js
+++ dist/index.js
@@ -11077,7 +11077,7 @@ const containsBumpTypeLabel = (labels) => {
     return false;
   }
 
-  return bumpTypes.some((bump_type) => labelsArray.includes(bump_type));
+  return bumpTypes.some((bumpType) => labelsArray.includes(bumpType));
 };
 
 const createBranch = async (octokit, context, branch) => {
@@ -11111,6 +11111,120 @@ const deleteBranch = async (octokit, context, branch) => {
     const response = await octokit.rest.git.deleteRef({
       ref: reference,
       ...context.repo,
+    });
+    return response;
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+const getChangedFiles = async (changedFiles) => {
+  const commitableFiles = [];
+  for (const path of changedFiles) {
+    let content = "";
+    const options = {
+      listeners: {
+        stdout: (data) => {
+          content += data.toString();
+        },
+      },
+    };
+
+    await exec.exec("cat", [path], options);
+
+    commitableFiles.push({
+      content,
+      path,
+      mode: "100644",
+      type: "commit",
+    });
+  }
+  return commitableFiles;
+};
+
+const createCommit = async (octokit, context, branchName) => {
+  try {
+    const branch = await octokit.rest.repos.getBranch({
+      ...context.repo,
+      branch: branchName,
+    });
+
+    const branchSha = branch.data.commit.sha;
+
+    const commits = await octokit.rest.repos.listCommits({
+      ...context.repo,
+      sha: branchSha,
+    });
+
+    const commitSHA = commits.data[0].sha;
+
+    let changedFiles = [];
+    const gitOptions = {};
+    gitOptions.listeners = {
+      stdout: (data) => {
+        const changedFilesString = data.toString();
+        changedFiles = changedFilesString
+          .split("\n")
+          .map((file) => file.split(" ")[2])
+          .filter(Boolean);
+      },
+    };
+    await exec.exec("git", ["status", "-s"], gitOptions);
+
+    const commitableFiles = await getChangedFiles(changedFiles);
+
+    const {
+      data: { sha: currentTreeSHA },
+    } = await octokit.rest.git.createTree({
+      ...context.repo,
+      tree: commitableFiles,
+      base_tree: commitSHA,
+      message: "Bumped gem version",
+      parents: [commitSHA],
+    });
+
+    const {
+      data: { sha: newCommitSHA },
+    } = await octokit.rest.git.createCommit({
+      ...context.repo,
+      tree: currentTreeSHA,
+      message: "Bumped gem version",
+      parents: [commitSHA],
+    });
+
+    await octokit.rest.git.updateRef({
+      ...context.repo,
+      sha: newCommitSHA,
+      ref: `heads/${branchName}`,
+    });
+
+    core.debug(`Commit created on the ${branchName} branch!`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+const createPR = async (octokit, context, branchName) => {
+  try {
+    const response = await octokit.rest.pulls.create({
+      head: branchName,
+      base: "main",
+      title: "Bump gem version",
+      body: "Bump gem version",
+      ...context.repo,
+    });
+    return response?.data?.number;
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+const mergePR = async (octokit, context, prNumber) => {
+  try {
+    const response = await octokit.rest.pulls.merge({
+      ...context.repo,
+      pull_number: prNumber,
+      merge_method: "squash",
     });
     return response;
   } catch (error) {
@@ -11153,21 +11267,31 @@ const run = async () => {
     core.info("Committing and pushing the new version...");
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
-    const branch = core.getInput("branch");
+    const branchName = core.getInput("branch");
     const context = github.context;
 
-    core.debug(`Creating branch ${branch}`);
-    let isCreated = await createBranch(octokit, context, branch);
+    core.debug(`Creating branch ${branchName}`);
+    let isBranchCreated = await createBranch(octokit, context, branchName);
 
-    console.log("Branch created:", Boolean(isCreated));
-    if (!Boolean(isCreated)) {
-      console.log("Deleting the existing branch...");
-      const isDeleted = await deleteBranch(octokit, context, branch);
-      console.log("Branch deleted:", Boolean(isDeleted));
-      console.log(`Creating a new branch ${branch}`);
-      isCreated = await createBranch(octokit, context, branch);
-      console.log("Branch created:", Boolean(isCreated));
+    core.debug("Branch created:", Boolean(isBranchCreated));
+    if (!Boolean(isBranchCreated)) {
+      core.debug("Deleting the existing branch...");
+      const isBranchDeleted = await deleteBranch(octokit, context, branchName);
+      core.debug("Branch deleted:", Boolean(isBranchDeleted));
+
+      core.debug(`Creating a new branch ${branchName}`);
+      isBranchCreated = await createBranch(octokit, context, branchName);
+      core.debug("Branch created:", Boolean(isBranchCreated));
     }
+
+    core.debug("Creating a commit...");
+    await createCommit(octokit, context, branchName);
+
+    core.debug("Creating a PR...");
+    const prNumber = await createPR(octokit, context, branchName);
+
+    core.debug("PR created:", Boolean(prNumber));
+    await mergePR(octokit, context, prNumber);
   } catch (error) {
     core.setFailed(error.message);
   }
